0d3a5374595e0171273360b405d30113be7d7c7e
Vinamra Jain <73946693+vjonline@users.noreply.github.com>
2024-08-25 19:29:24 +0530

    Create fibonacci_vj.cpp

---
+++ session1/fibonacci_vj.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+        int first=0; int second=1; int sum=0;
+
+        if(n==0 || n==1)
+            return(n);
+
+        else
+        {
+            for(int i=0;i<=n-1;i++)
+            {
+                    first=second;
+                    second=sum;
+                    sum=first+second;
+            }
+        }
+            return(sum);
+
+    }
+};
